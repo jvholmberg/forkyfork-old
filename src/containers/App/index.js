d80982ee273fca05cf43feea6f6f433f5213e28d
Johan Holmberg <johan.holmberg@sigma.se>
2018-05-11 16:21:24 +0200

    Created placeholder

---
+++ src/containers/App/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div id='heading'>
+          <div id='logo'>forkyfork</div>
+          <div id='tagline'>
+            Contact me at <a href='https://www.linkedin.com/in/johan-holmberg-70044bb6/'>LinkedIn</a></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
