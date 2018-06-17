54ac3cda309e934f7f038a8b2b07131148d53695
Johan Holmberg <johanholmberg@johans-mbp-2.mynet>
2018-06-17 21:04:39 +0200

    strip all content

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -21,81 +21,8 @@ class App extends Component {
       <div className="App app">
         <Hero>
           <h1>forkyfork</h1>
-          <p>makes you happy</p>
+          <p>enables your happiness</p>
         </Hero>
-        <NavStrip
-          position='left'
-          open={this.state.open}
-          action={(
-            <Fab
-              icon='fas fa-bars'
-              bgColor='#fff'
-              color='#33333d'
-              onClick={this.toggleNav} />
-          )}>
-          <Fab
-            icon='far fa-comment-alt'
-            bgColor='#fff'
-            color='#33333d'
-            title='Contact me' />
-          <Fab
-            icon='fas fa-code-branch'
-            bgColor='#fff'
-            color='#33333d'
-            title='Work' />
-          <Fab
-            icon='fas fa-chart-pie'
-            bgColor='#fff'
-            color='#33333d'
-            title='Skills' />
-          <Fab
-            icon='fas fa-ellipsis-h'
-            bgColor='#fff'
-            color='#33333d'
-            title='More' />
-        </NavStrip>
-        <Hero>
-          <h3>Who am i?</h3>
-          <p>
-            My name is Johan Holmberg and I live in Gothenburg where I'm working
-            at Sigma ITC as an Frontend developer. I specializes mainly in React
-            but as a fast learner who loves challenges I often venture outside
-            of the React ecosystem.
-          </p>
-
-        </Hero>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
       </div>
     );
   }
